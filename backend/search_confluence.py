cda4d2fd298a62f937cd209ff40bff9f5747c8b8
Ian <ianvzs@outlook.com>
2024-04-02 11:22:19 +0800

    fix search conflunce

--- backend/search_confluence.py
+++ backend/search_confluence.py
@@ -1,29 +1,33 @@
-
-#Copyright (c) Microsoft Corporation. All rights reserved.
-#Licensed under the MIT License.
-
 # -*- coding: utf-8 -*-
 
-import os 
+import os
 import requests
+from loguru import logger
 
 '''
-This sample makes a call to the Bing Web Search API with a query and returns relevant web search.
-Documentation: https://docs.microsoft.com/en-us/bing/search-apis/bing-web-search/overview
+This sample makes a call to the Confluence Web UI API with a query and returns relevant web search.
 '''
 
 class SearchConfluence:
-    # Add your Bing Search V7 subscription key and endpoint to your environment variables.
+    # Confluence have Confluence Data Center REST API but i use this web ui api test convenient.
     subscription_key = os.environ["CONFLUENCE_COOKIE"]
-    endpoint = os.environ['CONFLUENCE_HOST'] + "/confluence/rest/api/search"
+    endpoint = os.environ['CONFLUENCE_HOST'] + "/rest/api/search"
 
     def __init__(self):
         # Construct a request
-        self.headers = { 'cookie': SearchBing.subscription_key }
+        self.headers = { 'cookie': SearchConfluence.subscription_key }
 
     def search(self, query, mkt='en-US'):
         # Call the API
-        params = { 'q': query, 'mkt': mkt }
+        params = {
+            "cql": f'siteSearch ~ "{query}" AND type in ("space","user","com.atlassian.confluence.extra.team-calendars:calendar-content-type","attachment","page","com.atlassian.confluence.extra.team-calendars:space-calendars-view-content-type","blogpost")',
+            "start": 0,
+            "limit": 20,
+            "excerpt": "highlight",
+            "expand": "space.icon",
+            "includeArchivedSpaces": False,
+            "src": "next.ui.search"
+        }
         try:
             response = requests.get(self.endpoint, headers=self.headers, params=params)
             response.raise_for_status()
@@ -38,4 +42,5 @@ class SearchConfluence:
             else:
                 return "未检索到结果"
         except Exception as ex:
+            logger.error(f"Confluence {self.endpoint} error: {ex}")
             raise ex
